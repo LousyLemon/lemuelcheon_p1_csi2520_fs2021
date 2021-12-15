a085f4b690db594472f56c4414c1c2393e6abd68
Lemuel <lscheon@oakland.edu>
2021-12-15 16:49:39 -0500

    Fixed issues with CSS and images, nav bar goes through all pages

--- index.js
+++ index.js
@@ -25,14 +25,24 @@ app.use(express.urlencoded({extended:true})); //to parse html form data
 //Initialize ejs middleware
 app.set("view engine", "ejs");
 app.use("/public", express.static(path.join(__dirname, "public")));
+
 app.use(express.static('views'));
 //app.set("views", path.join(__dirname, "/views"));    //https://stackoverflow.com/questions/51844617/cannot-get-blah-html-ejs-cant-load-html/51845208
 
-
+//index page
 app.get("/", (req,res) => {
    res.render('index');
 });
 
+//about page
+app.get("/about", (req, res) => {
+  res.render('about');
+});
+
+//notifications page
+app.get('/notifications', (req, res) => {
+  res.render('notifications');
+});
 
 /*
 alpha.data.intraday(`tsla`).then((data) => {
@@ -58,7 +68,7 @@ var request = require('request');
 
 
 var url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=TSLA&interval=15min&apikey=2R4U55YO6G05KX6E';
-
+/*
 request.get({
     url: url,
     json: true,
@@ -75,13 +85,13 @@ request.get({
     }
 });
 tsla_dicts.forEach(logArrayElements)
-
-
+*/
+/*
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "root123",
-  database: "tsla" // comment out if running example 1
+ // database: "tsla" // comment out if running example 1
 });
 
 
@@ -93,6 +103,6 @@ db.connect((err) => {
     console.log(`Successful connected to the DB....`);
   }
 });
-
+*/
 
 app.listen(PORT, () => console.log('Server listening on port ' + PORT));
